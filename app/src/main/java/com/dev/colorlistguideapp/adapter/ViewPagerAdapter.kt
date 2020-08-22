0d979892173918eb8ede29bab3931d37f27671bd
NOntachai J <s5752410002@sau.ac.th>
2020-08-22 10:49:07 +0700

    add view pager and project structure.

---
+++ app/src/main/java/com/dev/colorlistguideapp/adapter/ViewPagerAdapter.kt
@@ -0,0 +1,31 @@
+package com.dev.colorlistguideapp.adapter
+
+import android.content.Context
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.bumptech.glide.load.engine.Resource
+import com.dev.colorlistguideapp.R
+import com.dev.colorlistguideapp.ui.ListColorFavFragment
+import com.dev.colorlistguideapp.ui.ListColorFragment
+
+class ViewPagerAdapter(val context: Context,fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager,BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    override fun getItem(position: Int): Fragment = when(position){
+        0 -> ListColorFragment()
+        else -> ListColorFavFragment()
+    }
+
+    override fun getCount(): Int = 2
+
+    override fun getItemPosition(`object`: Any): Int {
+        return super.getItemPosition(`object`)
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position){
+            0 ->  context.resources.getString(R.string.title_list)
+            else -> context.resources.getString(R.string.title_favorite)
+        }
+    }
+}
---
+++ app/src/main/java/com/dev/colorlistguideapp/ui/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.dev.colorlistguideapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.dev.colorlistguideapp.R
+import com.dev.colorlistguideapp.adapter.ViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ViewPagerAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initViewPager()
+
+    }
+
+    private fun initViewPager() {
+        adapter = ViewPagerAdapter(this, supportFragmentManager)
+        view_pager.adapter = adapter
+        tabs_main.setupWithViewPager(view_pager)
+    }
+}
