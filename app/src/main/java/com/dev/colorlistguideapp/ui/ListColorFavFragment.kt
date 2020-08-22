9449f75a181a811638345c9bd3fd31cb0697dad5
NOntachai J <s5752410002@sau.ac.th>
2020-08-22 13:21:19 +0700

    add callback func to swipe delete

--- app/src/main/java/com/dev/colorlistguideapp/ui/ListColorFavFragment.kt
+++ app/src/main/java/com/dev/colorlistguideapp/ui/ListColorFavFragment.kt
@@ -4,23 +4,26 @@ import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dev.colorlistguideapp.R
 import com.dev.colorlistguideapp.adapter.ColorAdapter
+import com.dev.colorlistguideapp.utils.SortType
 import com.dev.colorlistguideapp.viewModel.ColorFavoriteViewModel
 import kotlinx.android.synthetic.main.fragment_list_color_fav.*
 
 class ListColorFavFragment : Fragment() {
 
-    lateinit var colorAdapter : ColorAdapter
+    lateinit var colorAdapter: ColorAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
@@ -36,14 +39,31 @@ class ListColorFavFragment : Fragment() {
 
         val viewModel = ViewModelProvider(requireActivity()).get(ColorFavoriteViewModel::class.java)
 
-        viewModel.listColor.observe(viewLifecycleOwner, Observer { listData->
+        viewModel.listColor.observe(viewLifecycleOwner, Observer { listData ->
             Log.d(TAG, "onViewCreated: ${listData.filter { it.is_favorite }}")
 
-            colorAdapter = ColorAdapter(listData.filter { it.is_favorite }.toMutableList(),viewModel,false)
+            colorAdapter =
+                ColorAdapter(listData.filter { it.is_favorite }.toMutableList(), viewModel, false)
             recycler_view_list_fav.apply {
                 layoutManager = LinearLayoutManager(requireContext())
                 adapter = colorAdapter
             }
         })
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.menu_asc -> {
+            Toast.makeText(requireContext(), "ASC", Toast.LENGTH_SHORT).show()
+            colorAdapter.sortData(SortType.ASC)
+            true
+        }
+        R.id.menu_desc -> {
+            Toast.makeText(requireContext(), "DESC", Toast.LENGTH_SHORT).show()
+            colorAdapter.sortData(SortType.DESC)
+            true
+        }
+        else -> {
+            false
+        }
     }
 }
